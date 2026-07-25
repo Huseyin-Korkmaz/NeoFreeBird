d30d9560736f3c1530469641386d6335b07f5113
orionblur <252493356+orionblur@users.noreply.github.com>
2026-07-25 17:39:54 -0500

    Added detail to build script

--- build.sh
+++ build.sh
@@ -137,6 +137,7 @@ case "$MODE" in
         cyan -i packages/com.atebits.Tweetie2.ipa -o packages/NeoFreeBird-sideloaded --ignore-encrypted \
           -uwf .theos/obj/debug/zxPluginsInject.dylib .theos/obj/debug/libbhFLEX.dylib \
           .theos/obj/debug/BHTwitter.dylib layout/Library/Application\ Support/BHT/BHTwitter.bundle
+        say "Injecting OpenTwitterSafariExtension.appex into the IPA."  
         inject_appex packages/NeoFreeBird-sideloaded.ipa
       else
         say "Skipping cyan step because it is not installed."
@@ -165,6 +166,7 @@ case "$MODE" in
       if command -v cyan >/dev/null 2>&1; then
         cyan -i packages/com.atebits.Tweetie2.ipa -o packages/NeoFreeBird-trollstore.tipa --ignore-encrypted \
           -uwf .theos/obj/debug/BHTwitter.dylib .theos/obj/debug/libbhFLEX.dylib layout/Library/Application\ Support/BHT/BHTwitter.bundle
+        say "Injecting OpenTwitterSafariExtension.appex into the IPA."  
         inject_appex packages/NeoFreeBird-trollstore.tipa
       else
         say "Skipping cyan step because it is not installed."
